8db7e8fd14f4a5f4f7848f86540ceac01a585ae6
tom-anders <13141438+tom-anders@users.noreply.github.com>
2023-12-08 13:26:58 +0100

    feat: add find_into_tuple and find_parse_into_tuple

--- utils/src/regex_helper.rs
+++ utils/src/regex_helper.rs
@@ -5,14 +5,26 @@ use lazy_regex::regex;
 
 pub trait RegexHelper {
     /// shortcut for find_iter followed by a map to str
-    fn find_iter_str<'a, 'b: 'a>(&'a self, s: &'b str) -> impl Iterator<Item = &'a str> + 'a;
+    fn find_iter_str<'a, 'b: 'a>(&'a self, s: &'b str) -> impl Iterator<Item = &'b str> + 'a;
 
     fn find_iter_parse<'a, 'b: 'a, T: FromStr>(
         &'a self,
         s: &'b str,
     ) -> impl Iterator<Item = T> + 'a;
 
+    /// Find all matches and return them into a tuple
+    /// Panics if the number of matches does not match the tuple size
+    fn find_into_tuple<'a, 'b: 'a, T>(&'a self, s: &'b str) -> T
+    where
+        T: itertools::traits::HomogeneousTuple<Item = &'b str>;
+
+    fn find_parse_into_tuple<'a, 'b: 'a, T, I>(&'a self, s: &'b str) -> T
+    where
+        I: FromStr,
+        T: itertools::traits::HomogeneousTuple<Item = I>;
+
     /// Finds the first match and extracts its captures into a tuple
+    /// Panics if the number of captures does not match the tuple size
     fn capture_into_tuple<'a, 'b: 'a, T>(&'a self, s: &'b str) -> T
     where
         T: itertools::traits::HomogeneousTuple<Item = &'b str>;
@@ -35,7 +47,7 @@ where
 }
 
 impl RegexHelper for regex::Regex {
-    fn find_iter_str<'a, 'b: 'a>(&'a self, s: &'b str) -> impl Iterator<Item = &'a str> + 'a {
+    fn find_iter_str<'a, 'b: 'a>(&'a self, s: &'b str) -> impl Iterator<Item = &'b str> + 'a {
         self.find_iter(s).map(|m| m.as_str())
     }
 
@@ -43,10 +55,22 @@ impl RegexHelper for regex::Regex {
         &'a self,
         s: &'b str,
     ) -> impl Iterator<Item = I> + 'a {
-        self.find_iter_str(s).map(|s| {
-            s.parse()
-                .unwrap_or_else(|_| panic!("Failed to parse {}", s))
-        })
+        self.find_iter_str(s).map(|s| s.parse().unwrap_or_else(|_| panic!("Failed to parse {}", s)))
+    }
+
+    fn find_into_tuple<'a, 'b: 'a, T>(&'a self, s: &'b str) -> T
+    where
+        T: itertools::traits::HomogeneousTuple<Item = &'b str>,
+    {
+        self.find_iter_str(s).collect_tuple().expect("Number of matches does not equal tuple size")
+    }
+
+    fn find_parse_into_tuple<'a, 'b: 'a, T, I>(&'a self, s: &'b str) -> T
+    where
+        I: FromStr,
+        T: itertools::traits::HomogeneousTuple<Item = I>,
+    {
+        self.find_iter_parse(s).collect_tuple().unwrap()
     }
 
     fn capture_into_tuple<'a, 'b: 'a, T>(&'a self, s: &'b str) -> T
@@ -58,10 +82,7 @@ impl RegexHelper for regex::Regex {
             .iter()
             .skip(1) // skip whole match, only return captures
             .enumerate()
-            .map(|(i, c)| {
-                c.unwrap_or_else(|| panic!("No match for group {i}"))
-                    .as_str()
-            })
+            .map(|(i, c)| c.unwrap_or_else(|| panic!("No match for group {i}")).as_str())
             .collect_tuple()
             .expect("Num captures should match tuple size")
     }
@@ -76,14 +97,8 @@ impl RegexHelper for regex::Regex {
             .iter()
             .skip(1) // skip whole match, only return captures
             .enumerate()
-            .map(|(i, c)| {
-                c.unwrap_or_else(|| panic!("No match for group {i}"))
-                    .as_str()
-            })
-            .map(|s| {
-                s.parse::<I>()
-                    .unwrap_or_else(|_| panic!("Failed to parse {}", s))
-            })
+            .map(|(i, c)| c.unwrap_or_else(|| panic!("No match for group {i}")).as_str())
+            .map(|s| s.parse::<I>().unwrap_or_else(|_| panic!("Failed to parse {}", s)))
             .collect_tuple()
             .expect("Num captures should match tuple size")
     }
@@ -95,26 +110,14 @@ mod tests {
 
     #[test]
     fn find_iter() {
-        itertools::assert_equal(
-            regex!(r"\d+").find_iter_str("123 456 789"),
-            ["123", "456", "789"],
-        );
-        itertools::assert_equal(
-            [123, 456, 789],
-            regex!(r"\d+").find_iter_parse("123 456 789"),
-        );
+        itertools::assert_equal(regex!(r"\d+").find_iter_str("123 456 789"), ["123", "456", "789"]);
+        itertools::assert_equal([123, 456, 789], regex!(r"\d+").find_iter_parse("123 456 789"));
     }
 
     #[test]
     fn numbers() {
-        assert_eq!(
-            vec![123, 456, 789],
-            extract_numbers("123 456 789").collect_vec()
-        );
-        assert_eq!(
-            vec![-123, 456, -789],
-            extract_numbers("-123   456 -789").collect_vec()
-        );
+        assert_eq!(vec![123, 456, 789], extract_numbers("123 456 789").collect_vec());
+        assert_eq!(vec![-123, 456, -789], extract_numbers("-123   456 -789").collect_vec());
     }
 
     #[test]
@@ -135,5 +138,11 @@ mod tests {
             (123_usize, 456_usize),
             regex!(r"(\d+)\s*(\d+)").capture_parse_into_tuple("  123 456")
         );
+    }
+
+    #[test]
+    fn find_into_tuple() {
+        assert_eq!(("hello", "world", "bye"), regex!(r"\w+").find_into_tuple("hello world !!! bye"));
+        assert_eq!((1, -1, 22), regex!(r"[+-]?\d+").find_parse_into_tuple("hjkhkjh1 hhjk -1hjhhhh [[22]]"));
     }
 }
